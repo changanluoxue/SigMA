53126765350d6eded7b75156356c43c4081d86b3
changanluoxue <81601152+changanluoxue@users.noreply.github.com>
2026-04-03 11:13:46 +0800

    Update example1_stride.py

--- example1_stride.py
+++ example1_stride.py
@@ -11,7 +11,7 @@ import torch.optim as optim
 import utils
 import NNmodels
 
-'''自定义损失函数'''
+'''Custom loss function'''
 def rmse_loss_fn(y_pre, y):
     return torch.sqrt(F.mse_loss(y_pre, y))
 
@@ -19,7 +19,7 @@ __name__ = "__results__" # "__training__", "__results__"
 
 if __name__ == "__training__":
     start = time.time()
-    '''参数输入'''
+    '''Parameter input'''
     models = 'fBm'  # 'fBm', 'fOU', 'rHeston'
     grid_points = 100  # 100, 500, 1000, 1500
     max_epochs = 150
@@ -27,7 +27,7 @@ if __name__ == "__training__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print('device:', device)
 
-    '''导入数据集并处理'''
+    '''Import and process dataset'''
     X_train = np.load(f'data/simulated_data/{models}/Xtrain_single_grid={grid_points}.npy')
     Y_train = np.load(f'data/simulated_data/{models}/Ytrain_single_grid={grid_points}.npy')
     X_eval = np.load(f'data/simulated_data/{models}/Xeval_single_grid={grid_points}.npy')
@@ -36,33 +36,33 @@ if __name__ == "__training__":
     train_dataloader, eval_dataloader, example_batch_x, example_batch_y = utils.generate_torch_batched_data(X_train, Y_train, X_eval, Y_eval,
                                                                                                             train_batch_size=64, test_batch_size=64, device=device)
 
-    '''定义训练器'''
+    '''Define trainer'''
     model_trainer = utils.create_model_supervised_trainer(max_epochs=max_epochs, optimizer_fn=optimizer_fn,
                                                           loss_fn=rmse_loss_fn, train_dataloader=train_dataloader,
                                                           eval_dataloader=eval_dataloader, example_batch_x=example_batch_x, lr=lr, device=device)
 
-    '''训练模型'''
+    '''Train models'''
     if grid_points == 100:
         name = [r'$\rm SigMA_{stride=1}$', r'$\rm SigMA_{stride=10}$', r'$\rm SigMA_{stride=50}$']
         for round in range(3):
             history={}
-            print('******开始训练SigMA_stride1******')
+            print('******Starting SigMA_stride1 training******')
             sigma_stride1 = NNmodels.sigma_stride(augment_include_original=True, augment_include_time=True, T=1, grid_points=grid_points, stride=1).to(device)
             model_trainer(sigma_stride1, name[0], history)
             torch.save(sigma_stride1, f'data/results/numerical_example1/trained_models/{models}/sigma_length{grid_points}_stride1_round{round}.pth')
-            print('******SigMA_stride1训练完成******')
+            print('******SigMA_stride1 training complete******')
 
-            print('******开始训练SigMA_stride10******')
+            print('******Starting SigMA_stride10 training******')
             sigma_stride10 = NNmodels.sigma_stride(augment_include_original=True, augment_include_time=True, T=1, grid_points=grid_points, stride=10).to(device)
             model_trainer(sigma_stride10, name[1], history)
             torch.save(sigma_stride10, f'data/results/numerical_example1/trained_models/{models}/sigma_length{grid_points}_stride10_round{round}.pth')
-            print('******SigMA_stride10训练完成******')
+            print('******SigMA_stride10 training complete******')
 
-            print('******开始训练SigMA_stride50******')
+            print('******Starting SigMA_stride50 training******')
             sigma_stride50 = NNmodels.sigma_stride(augment_include_original=True, augment_include_time=True, T=1, grid_points=grid_points, stride=50).to(device)
             model_trainer(sigma_stride50, name[2], history)
             torch.save(sigma_stride50, f'data/results/numerical_example1/trained_models/{models}/sigma_length{grid_points}_stride50_round{round}.pth')
-            print('******SigMA_stride50训练完成******')
+            print('******SigMA_stride50 training complete******')
 
             with open(f'data/results/numerical_example1/history/{models}/history_length{grid_points}_round{round}.pkl', 'wb') as f:
                 pickle.dump(history, f)
@@ -71,36 +71,36 @@ if __name__ == "__training__":
         name = [r'$\rm SigMA_{stride=50}$', fr'$\rm SigMA_{{stride={int(grid_points/2)}}}$']
         for round in range(3):
             history = {}
-            print('******开始训练SigMA_stride50******')
+            print('******Starting SigMA_stride50 training******')
             sigma_stride50 = NNmodels.sigma_stride(augment_include_original=True, augment_include_time=True, T=1, grid_points=grid_points, stride=50).to(device)
             model_trainer(sigma_stride50, name[0], history)
             torch.save(sigma_stride50, f'data/results/numerical_example1/trained_models/{models}/sigma_length{grid_points}_stride50_round{round}.pth')
-            print('******SigMA_stride50训练完成******')
+            print('******SigMA_stride50 training complete******')
 
-            print('******开始训练SigMA_stridehalf******')
+            print('******Starting SigMA_stridehalf training******')
             sigma_stridehalf = NNmodels.sigma_stride(augment_include_original=True, augment_include_time=True, T=1, grid_points=grid_points, stride=int(grid_points/2)).to(device)
             model_trainer(sigma_stridehalf, name[1], history)
             torch.save(sigma_stridehalf, f'data/results/numerical_example1/trained_models/{models}/sigma_length{grid_points}_stridehalf_round{round}.pth')
-            print('******SigMA_stridehalf训练完成******')
+            print('******SigMA_stridehalf training complete******')
 
             with open(f'data/results/numerical_example1/history/{models}/history_length{grid_points}_round{round}.pkl', 'wb') as f:
                 pickle.dump(history, f)
 
     end = time.time()
-    print('---------------总耗时 {:.2f}s---------------'.format(end-start))
+    print('---------------Total time elapsed {:.2f}s---------------'.format(end-start))
 
 if __name__ == '__results__':
-    '''参数输入'''
+    '''Parameter input'''
     models = 'rHeston'  # 'fBm', 'fOU' , 'rHeston'
     grid_points = 1500  # 100, 500, 1000, 1500
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    '''导入数据集'''
+    '''Import datasetOutput training results'''
     history = []
     for round in range(3):
         with open(f'data/results/numerical_example1/history/{models}/history_length{grid_points}_round{round}.pkl', 'rb') as f:
             history.append(pickle.load(f))
-    '''输出训练结果'''
-    '打印表格'
+    '''Output training results'''
+    'Print summary table'
     print(f"{models}_length{grid_points}:")
     params = {}
     if grid_points == 100:
@@ -110,7 +110,7 @@ if __name__ == '__results__':
         for i, j in zip((r'$\rm SigMA_{stride=1}$', r'$\rm SigMA_{stride=10}$', r'$\rm SigMA_{stride=50}$'),
                         (sigma_stride1, sigma_stride10, sigma_stride50)):
             params[i] = utils.count_parameters(j)
-        '各轮表格'
+        'Per-round tables'
         for round in range(3):
             table_data = []
             for key in history[round]:
@@ -118,7 +118,7 @@ if __name__ == '__results__':
             print(f"Round{round}:")
             print(tabulate(table_data, headers=['Model', 'Eval MSE', 'Eval RMSE', '# Parameters'],
                            tablefmt='grid', floatfmt=['', '.3e', '.3e', '']))
-        '平均表格'
+        'Averaged'
         Amse, Armse, C = defaultdict(float), defaultdict(float), defaultdict(int)
         for round in range(3):
             for k, v in history[round].items():
@@ -133,7 +133,7 @@ if __name__ == '__results__':
         for i, j in zip((r'$\rm SigMA_{stride=50}$', fr'$\rm SigMA_{{stride={int(grid_points/2)}}}$'),
                         (sigma_stride50, sigma_stridehalf)):
             params[i] = utils.count_parameters(j)
-        '各轮表格'
+        'Per-round tables'
         for round in range(3):
             table_data = []
             for key in history[round]:
@@ -141,7 +141,7 @@ if __name__ == '__results__':
             print(f"Round{round}:")
             print(tabulate(table_data, headers=['Model', 'Eval MSE', 'Eval RMSE', '# Parameters'],
                            tablefmt='grid', floatfmt=['', '.2e', '.2e', '']))
-        '平均表格'
+        'Averaged'
         Amse, Armse, C = defaultdict(float), defaultdict(float), defaultdict(int)
         for round in range(3):
             for k, v in history[round].items():
@@ -150,7 +150,7 @@ if __name__ == '__results__':
         print("Average:")
         print(tabulate(Arows, headers=['Model', 'Eval MSE', 'Eval RMSE', '# Parameters'],
                        tablefmt='grid', floatfmt=['','.2e','.2e','']))
-    '绘制图形'
+    'Plot results'
     def plot_loss_descent(grid_points=None, name=None, history=None):
         colors = np.array([[0.5       , 0.5       , 0.5       , 1.        ],
                            [0.        , 0.64509804, 1.        , 1.        ],
